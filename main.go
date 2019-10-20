6f56bd0af1a6bd5d4420bcd3d4160e678d84450f
Artur Nerkowski <a.apatryda@gmail.com>
2019-10-20 12:18:08 +0200

    Initial implementation

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func getFileEnvMap() map[string]string {
+	fileEnvMap := make(map[string]string)
+
+	for _, env := range os.Environ() {
+		nameVal := strings.SplitN(env, "=", 2)
+		if strings.HasSuffix(nameVal[0], "_FILE") {
+			fileEnvMap[nameVal[0]] = nameVal[1]
+		}
+	}
+
+	return fileEnvMap
+}
+
+func readFileEnvs(fileEnvMap map[string]string) map[string]string {
+	readFileEnvMap := make(map[string]string)
+
+	for fileEnvName, filePath := range fileEnvMap {
+		envValue, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			envName := strings.TrimSuffix(fileEnvName, "_FILE")
+			readFileEnvMap[envName] = string(envValue)
+		}
+	}
+
+	return readFileEnvMap
+}
+
+func buildEnvs(envMap map[string]string) []string {
+	var envs []string
+
+	for name, value := range envMap {
+		envs = append(envs, name+"="+value)
+	}
+
+	return envs
+}
+
+func main() {
+	fileEnvMap := getFileEnvMap()
+	log.Println(fileEnvMap)
+
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd.Env = append(os.Environ(), buildEnvs(readFileEnvs(fileEnvMap))...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
